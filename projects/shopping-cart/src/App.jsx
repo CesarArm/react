45a487f07ca5a47d2d53d504327701be871227e3
cesar pinillos <cesarpinillos18gmail.com>
2023-12-07 16:42:45 -0500

    first commit

--- projects/shopping-cart/src/App.jsx
+++ projects/shopping-cart/src/App.jsx
@@ -1,11 +1,31 @@
+import { useState } from "react"
 import { Products } from "./components/Products" 
 import { products } from './mocks/products.json'
 
 function App() {
 
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0
+  })
+
+  const filterProducts = (products) => {
+    return products.filter(product => {
+      return (
+        product.price >= filterProducts.minPrice &&
+        (
+          filters.category == 'all' ||
+          product.category == filters.category
+        )
+      )
+    })
+  }
+
+  const filteredProducts = filterProducts(products)
+
   return (
     <>
-      <Products products={products} />
+      <Products products={filteredProducts} />
     </>
   )
 }
